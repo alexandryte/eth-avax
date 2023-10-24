ef17c3b94baa78b4fe6eba23c40501a6294fbd81
alexandryte <75197322+alexandryte@users.noreply.github.com>
2023-10-24 22:11:45 +0800

    Create eth-avax-panela.sol

---
+++ eth-avax-panela.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract errorcontract {
+    uint256 public value;
+
+    function setValue(uint256 _newValue) public {
+        require(_newValue > value, "New value must be greater than current value");
+        value = _newValue;
+    }
+
+    function assertExample(uint256 _input) public pure returns (uint256) {
+        assert(_input != 0);
+        return _input * 2;
+    }
+
+    function revertExample(uint256 a, uint256 b) public pure returns (string memory) {
+        // Adding a condition (a == b) before reverting
+        if (a == b) {
+            revert("a must not be equal to b, reverting transaction");
+        }
+
+        return "Transaction successful";
+    }
+}
